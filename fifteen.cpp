7cb55e54eef2fe47a03c23e5f678e132f155a09e
henrygenus <henry.a.genus@gmail.com>
2020-09-16 15:14:20 -0700

    Update fifteen.cpp

--- fifteen.cpp
+++ fifteen.cpp
@@ -23,12 +23,14 @@ int main() {
 
 
     char ch=0;
-    std::cout << "Press Q to quit and W, A, S, or D to move.\n";
-    testerBoard.printBoard();
-    while (ch != 'Q' && ch!='q') {
+    
+    do {
+        std::cout << "\033[2J\033[1;1H";
+        std::cout << "Press Q to quit and W, A, S, or D to move.\n";
+        testerBoard.printBoard();
 
         ch = std::getchar();
-
+        
         if (testerBoard.isSolved()) {
             std::cout << "Game over, you won! \n";
             return 0;
