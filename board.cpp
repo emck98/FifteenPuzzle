9752fabc6e862c6313592832ccdc34c90cf612c3
henrygenus <henry.a.genus@gmail.com>
2020-09-16 23:57:03 -0700

    remove hardcoded initial array

--- board.cpp
+++ board.cpp
@@ -5,11 +5,10 @@
 #include <chrono>      
 
 
-std::array<int, SIZE> sorted_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0}; 
-
 Board::Board()
-: state(sorted_array)
-{}
+{
+    std::iota(this->state.begin(), this->state.end(), 0);
+}
 
 void Board::printBoard() {
 
