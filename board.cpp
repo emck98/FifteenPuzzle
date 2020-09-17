f6d6b9de58daabb6a8dd3fdb11d71dfe5e219508
henrygenus <henry.a.genus@gmail.com>
2020-09-17 00:14:42 -0700

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -177,36 +177,6 @@ char intToChar(int tile) {
         return 'A' + tile - 10;
     else
         return '*';
-    /*
-    char ret;
-    switch(tile) {
-        case 0:
-            ret = ' ';
-            break;
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            ret = tile + '0';
-            break;
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-            ret = 'A' + (tile - 10);
-            break;
-        default:
-              ret = '*';
-    }
-    return ret;
-     */
 }
 
 
