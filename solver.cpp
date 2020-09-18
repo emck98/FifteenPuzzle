ffe85565916325f0a3cc9a3fffa4f119be671b40
henrygenus <henry.a.genus@gmail.com>
2020-09-18 02:14:22 -0700

    works but slow

--- solver.cpp
+++ solver.cpp
@@ -20,47 +20,40 @@ using heap = std::priority_queue<
     std::vector< std::pair<Board*, int> >,
     comparator>;
 
-int aStar(heap q, std::unordered_set<Board> h) {
+int aStar(heap q) {
     Board* b = q.top().first;
     int moves = q.top().second;
     q.pop();
-    std::cout << moves << std::endl;
     if (b->ManhattanDistance() == 0)
         return moves;
     else {
-        b->up();
-        if (h.find(*b) == h.end()) {
-            h.insert(*b);
-            q.push(std::make_pair(new Board(*b), moves+1));
-        }
-        b->down();
-        b->right();
-        if (h.find(*b) == h.end()) {
-            h.insert(*b);
-            q.push(std::make_pair(new Board(*b), moves+1));
-        }
-        b->left();
-        b->down();
-        if (h.find(*b) == h.end()) {
-            h.insert(*b);
-            q.push(std::make_pair(new Board(*b), moves+1));
-        }
-        b->up();
-        b->left();
-        if (h.find(*b) == h.end()) {
-            h.insert(*b);
-            q.push(std::make_pair(new Board(*b), moves+1));
-        }
-        return aStar(q, h);
+        Board* bprime = new Board(*b);
+        bprime->up();
+        if (! (*bprime == *b))
+            q.push(std::make_pair(bprime, moves+1));
+        
+        bprime = new Board(*b);
+        bprime->right();
+        if (! (*bprime == *b))
+            q.push(std::make_pair(bprime, moves+1));
+        
+        bprime = new Board(*b);
+        bprime->down();
+        if (! (*bprime == *b))
+            q.push(std::make_pair(bprime, moves+1));
+        
+        bprime = new Board(*b);
+        bprime->left();
+        if (! (*bprime == *b))
+            q.push(std::make_pair(bprime, moves+1));
+        return aStar(q);
     }
 }
 
 int solve(Board b) {
     heap q;
     q.push(std::make_pair(new Board(b), 0));
-    std::unordered_set<Board> h;
-    h.insert(b);
-    return aStar(q, h);
+    return aStar(q);
 }
 
 
