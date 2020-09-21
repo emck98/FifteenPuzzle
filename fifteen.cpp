c383ab4b07796f7a452d443baba76df96be1bdf0
henrygenus <henry.a.genus@gmail.com>
2020-09-20 21:51:24 -0700

    Update fifteen.cpp

--- fifteen.cpp
+++ fifteen.cpp
@@ -7,22 +7,26 @@
 
 int main() {
 
-    Board testerBoard(4);
+    Board testerBoard(3);
     std::system("clear");
+    int minMoves = solve(testerBoard).second;
     
-    //std::cout << "Win in " << solve(testerBoard).second << " moves" << std::endl;
-
-    char ch;
+    int ch, moveNo = 1;
     do {
-
+        
+        std::cout << "Win in " << minMoves << " moves!" << std::endl;
         testerBoard.printBoard();
+        
         if (testerBoard.isSolved()) {
             std::cout << "Game over, you won!" << std::endl;
             return 0;
         }
 
         std::cout << "Press Q to quit and W, A, S, or D to move.\n";
+        std::cout << "Move " << moveNo << ": ";
+        
         ch = std::getchar();
+        fflush(stdin); // handle 'RETURN' key or multiple keys
 
         switch(ch)  {
             case 'W':
@@ -41,8 +45,10 @@ int main() {
             case 'd':
                 testerBoard.right();
                 break;
-
+            default:
+                continue;
         }
+        moveNo++;
         std::system("clear");
     } while (ch != 'Q' && ch!='q');
   
